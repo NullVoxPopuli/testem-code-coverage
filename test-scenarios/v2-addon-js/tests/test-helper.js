75f899a4eee9f310704387a3d9ed52ba52802481
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-03-15 23:23:35 -0400

    ope

--- test-scenarios/v2-addon-js/tests/test-helper.js
+++ test-scenarios/v2-addon-js/tests/test-helper.js
@@ -4,7 +4,7 @@ import * as QUnit from "qunit";
 import { setApplication } from "@ember/test-helpers";
 import { setup } from "qunit-dom";
 import { start as qunitStart, setupEmberOnerrorValidation } from "ember-qunit";
-import { setupCoverage } from "testem-code-coverage/testem";
+import { setupCoverage } from "testem-code-coverage/runtime";
 
 class Router extends EmberRouter {
   location = "none";
--- test-scenarios/vite-app-js/tests/test-helper.js
+++ test-scenarios/vite-app-js/tests/test-helper.js
@@ -5,7 +5,7 @@ import * as QUnit from "qunit";
 import { setApplication } from "@ember/test-helpers";
 import { setup } from "qunit-dom";
 import { start as qunitStart, setupEmberOnerrorValidation } from "ember-qunit";
-import { setupCoverage } from "testem-code-coverage/testem";
+import { setupCoverage } from "testem-code-coverage/runtime";
 
 export function start() {
   setApplication(Application.create(config.APP));
