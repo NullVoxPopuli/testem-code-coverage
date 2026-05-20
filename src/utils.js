f7d1e5f7bd1506ccf2ea218da7300448626f6024
Forrest <forrest.deters@gmail.com>
2026-05-20 13:58:39 -0700

    Use node:path util for posix path coersion

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
+import path from "node:path";
+
 export const REPORT_TO_MIDDLEWARE_PATH = "/__testem-code-coverage__/coverage";
 
 /** Normalize path separators to POSIX-style forward slashes. */
 export function toPosixPath(filePath) {
-  return String(filePath).replace(/\\/g, "/");
+  return path.posix.normalize(String(filePath));
 }
